da67eb218c60573155294df7e84535bbc3a91b0d
mromeroruiz <mromeroruiz.business@gmail.com>
2019-02-04 09:49:00 -0500

    Delete controller

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -95,6 +95,21 @@ namespace ElevenNote.Services
             }
         }
 
+        public bool DeleteNote(int noteId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == noteId && e.OwnerId == _userId);
+
+                ctx.Notes.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
 
     }
 }
